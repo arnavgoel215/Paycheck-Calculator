96857c7970d7499af481df4f83a8891f18bfd464
Arnav Goel <arnavgoel13@gmail.com>
2025-03-09 16:12:58 -0500

    Rewrite of code

--- calculator.py
+++ calculator.py
@@ -1,12 +1,5 @@
 import zmq
 
-def calculate_wage(hourly_rate: float, hours_worked: int) -> float:
-    return hourly_rate * hours_worked
-
-def calculate_salary(yearly_salary: float) -> float:
-    weekly_rate = yearly_salary / 52  # dividing by 52 since there are 52 weeks in a year
-    return round(weekly_rate, 2)
-
 def calculator_server():
     context = zmq.Context()
     socket = context.socket(zmq.REP)
@@ -15,26 +8,12 @@ def calculator_server():
     print("Server is running on port 5555....")
 
     while True:
-        message = socket.recv_string()
-        if message == "CALC_SALARY":
-            try:
-                yearly_salary = float(socket.recv_string())
-                print(f"Received a yearly salary value of {yearly_salary}")
-                total_pay = calculate_salary(yearly_salary)
-                socket.send_string(str(total_pay))
-            except ValueError:
-                socket.send_string("Error: Received invalid value")
-        elif message == "CALC_WAGE":
-            try:
-                hourly_rate = float(socket.recv_string())
-                hours_worked = int(socket.recv_string())
-                print(f"Received hourly rate value of {hourly_rate} and working hours of {hours_worked}")
-                total_pay = calculate_wage(hourly_rate, hours_worked)
-                socket.send_string(str(total_pay))
-            except ValueError:
-                socket.send_string("Error: Received invalid value")
-        else:
-            socket.send_string("Error: Invalid command")
+        message = socket.recv_json()
+        hours = message.get("hours", 0)
+        pay_rate = message.get("pay_rate", 0)
+        total_pay = hours * pay_rate
+
+        socket.send_string(str(total_pay))
 
 
 if __name__ == "__main__":
